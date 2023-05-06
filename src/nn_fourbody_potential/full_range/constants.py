0edbdced1c86076f561da2cd4e17974073046d98
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-05-06 00:32:58 -0400

    Add global constants related to extrapolation

---
+++ src/nn_fourbody_potential/full_range/constants.py
@@ -0,0 +1,7 @@
+"""
+This module contains constants relevant to determining whether a four-body geometry
+falls into the short-range, mid-range, or long-range categories.
+"""
+
+SHORT_RANGE_DISTANCE_CUTOFF = 2.2
+LONG_RANGE_SUM_OF_SIDELENGTHS_CUTOFF = 3.3 * 6
