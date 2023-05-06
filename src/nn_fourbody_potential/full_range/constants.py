8d0b935ba9efa5bd592df1f0e96a4bf146781ba4
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-05-06 17:05:18 -0400

    Make the short range scaling step a suggested global constant

--- src/nn_fourbody_potential/full_range/constants.py
+++ src/nn_fourbody_potential/full_range/constants.py
@@ -3,5 +3,6 @@ This module contains constants relevant to determining whether a four-body geome
 falls into the short-range, mid-range, or long-range categories.
 """
 
+SHORT_RANGE_SCALING_STEP = 0.05
 SHORT_RANGE_DISTANCE_CUTOFF = 2.2
 LONG_RANGE_SUM_OF_SIDELENGTHS_CUTOFF = 3.3 * 6
