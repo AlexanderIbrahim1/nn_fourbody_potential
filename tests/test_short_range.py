635840d0b940ae1fbd255a280439366d3a6340fa
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-05-06 20:40:09 -0400

    More unit tests to make sure everything works

--- tests/test_short_range.py
+++ tests/test_short_range.py
@@ -4,7 +4,7 @@ from typing import Sequence
 
 from nn_fourbody_potential.full_range.constants import SHORT_RANGE_DISTANCE_CUTOFF
 from nn_fourbody_potential.full_range.constants import SHORT_RANGE_SCALING_STEP
-from nn_fourbody_potential.full_range.short_range import prepare_extrapolation_data
+from nn_fourbody_potential.full_range.short_range import prepare_short_range_extrapolation_data
 
 
 class Test_prepare_extrapolation_data:
@@ -13,7 +13,7 @@ class Test_prepare_extrapolation_data:
         scaling_step = 0.05
         short_range_cutoff = 2.0
 
-        extrap_sample, extrap_dist_info = prepare_extrapolation_data(
+        extrap_sample, extrap_dist_info = prepare_short_range_extrapolation_data(
             sample, scaling_step, short_range_cutoff=short_range_cutoff
         )
 
@@ -25,26 +25,26 @@ class Test_prepare_extrapolation_data:
 
         assert _is_sequence_pytest_approx_equal(extrap_sample.lower, expect_sample_min)
         assert _is_sequence_pytest_approx_equal(extrap_sample.upper, expect_sample_mid)
+        assert extrap_dist_info.r_short_range == pytest.approx(expect_r_short_range)
         assert extrap_dist_info.r_lower == pytest.approx(expect_r_lower)
         assert extrap_dist_info.r_upper == pytest.approx(expect_r_upper)
-        assert extrap_dist_info.r_short_range == pytest.approx(expect_r_short_range)
 
     @pytest.mark.parametrize("invalid_scaling_step", [0.0, -0.05])
     def test_raises_nonpositive_scaling_step(self, invalid_scaling_step: float) -> None:
         arb_sample = (1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
         with pytest.raises(ValueError):
-            prepare_extrapolation_data(arb_sample, invalid_scaling_step, short_range_cutoff=SHORT_RANGE_DISTANCE_CUTOFF)
+            prepare_short_range_extrapolation_data(arb_sample, invalid_scaling_step, short_range_cutoff=SHORT_RANGE_DISTANCE_CUTOFF)
 
     def test_raises_sidelengths_to_large(self) -> None:
         too_large_sample = tuple([SHORT_RANGE_DISTANCE_CUTOFF * 1.05] * 6)
         with pytest.raises(ValueError):
-            prepare_extrapolation_data(too_large_sample, SHORT_RANGE_SCALING_STEP, SHORT_RANGE_DISTANCE_CUTOFF)
+            prepare_short_range_extrapolation_data(too_large_sample, SHORT_RANGE_SCALING_STEP, SHORT_RANGE_DISTANCE_CUTOFF)
 
     @pytest.mark.parametrize("invalid_sidelength", [0.0, -0.05])
     def test_raises_nonpositive_sidelength(self, invalid_sidelength: float) -> None:
         sample_with_invalid_sidelength = (invalid_sidelength, 1.0, 1.0, 1.0, 1.0, 1.0)
         with pytest.raises(ValueError):
-            prepare_extrapolation_data(
+            prepare_short_range_extrapolation_data(
                 sample_with_invalid_sidelength,
                 SHORT_RANGE_SCALING_STEP,
                 SHORT_RANGE_DISTANCE_CUTOFF,
---
+++ tests/test_short_range_extrapolation_types.py
@@ -0,0 +1,50 @@
+import pytest
+
+import numpy as np
+
+from typing import Sequence
+
+from nn_fourbody_potential.full_range.constants import SHORT_RANGE_DISTANCE_CUTOFF
+from nn_fourbody_potential.full_range.constants import SHORT_RANGE_SCALING_STEP
+from nn_fourbody_potential.full_range.short_range import prepare_short_range_extrapolation_data
+
+from nn_fourbody_potential.full_range.short_range_extrapolation_types import ExtrapolationDistanceInfo
+from nn_fourbody_potential.full_range.short_range_extrapolation_types import ExtrapolationEnergies
+from nn_fourbody_potential.full_range.short_range_extrapolation_types import LinearEnergyExtrapolator
+from nn_fourbody_potential.full_range.short_range_extrapolation_types import ExponentialEnergyExtrapolator
+
+
+class TestLinearEnergyExtrapolator:
+    def test_basic(self):
+        energies = ExtrapolationEnergies(1.0, 0.0)
+        extrap_distance_info = ExtrapolationDistanceInfo(0.0, 1.0, 2.0)
+
+        expect_slope = -1.0
+        expect_energy = 2.0
+
+        linear_extrapolator = LinearEnergyExtrapolator(energies, extrap_distance_info)
+
+        assert linear_extrapolator.slope == pytest.approx(expect_slope)
+        assert linear_extrapolator.energy == pytest.approx(expect_energy)
+
+
+class TestExponentialEnergyExtrapolator:
+    def test_basic(self):
+        def expon_func(x: float, coeff: float = 1.0, expon: float = 1.0) -> float:
+            return coeff * np.exp(-expon * x)
+
+        r_short_range = 1.0
+        r_lower = 3.0
+        r_upper = 4.0
+
+        energies = ExtrapolationEnergies(expon_func(r_lower), expon_func(r_upper))
+        extrap_distance_info = ExtrapolationDistanceInfo(r_short_range, r_lower, r_upper)
+
+        expect_slope = 1.0
+        expect_energy = expon_func(r_short_range)
+
+        expon_extrapolator = ExponentialEnergyExtrapolator(energies, extrap_distance_info)
+
+        assert expon_extrapolator.slope == pytest.approx(expect_slope)
+        assert expon_extrapolator.energy == pytest.approx(expect_energy)
+        assert not expon_extrapolator.is_magnitude_increasing_with_distance
