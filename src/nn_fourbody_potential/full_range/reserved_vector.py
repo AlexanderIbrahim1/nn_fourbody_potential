e1d8dd8c55a3a58b32cb46c75248f40184821fc0
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-05-16 23:30:13 -0400

    Move ReservedVector tests to their own module; add push/pop functionality

--- src/nn_fourbody_potential/full_range/reserved_vector.py
+++ src/nn_fourbody_potential/full_range/reserved_vector.py
@@ -18,23 +18,67 @@ T = TypeVar("T")
 
 
 class ReservedVector(Generic[T]):
-    def __init__(self, shape: Union[int, Sequence[int]], type_t: T) -> None:
+    def __init__(self, elements: np.ndarray[T], i_elem_start: int, i_elem_end: int) -> None:
         """
         NOTE: I cannot set `dtype=T` for the numpy array because it will throw an error such as
             `TypeError: Cannot interpret '~T' as a data type`
         This is why the `type_t` variable must be redundantly passed into the constructor
 
         """
-        self._elements = np.empty(shape, dtype=type_t)
-        self._i_elem = 0
+        self._elements = elements
+        self._i_elem_start = i_elem_start
+        self._i_elem_end = i_elem_end
+    
+    @classmethod
+    def new(cls, shape: Union[int, Sequence[int]], type_t: T) -> 'ReservedVector[T]':
+        """
+        NOTE: I cannot set `dtype=T` for the numpy array because it will throw an error such as
+            `TypeError: Cannot interpret '~T' as a data type`
+        This is why the `type_t` variable must be redundantly passed into the constructor
+
+        """
+        i_elem_start = 0
+        i_elem_end = 0
+        return cls(np.empty(shape, dtype=type_t), i_elem_start, i_elem_end)
+    
+    @classmethod
+    def from_array(cls, elements: np.ndarray[T]) -> 'ReservedVector[T]':
+        i_elem_start = 0
+        i_elem_end = len(elements)
+        return cls(elements, i_elem_start, i_elem_end)
 
     def push_back(self, elem: T) -> None:
-        if self._i_elem >= self._elements.size:
-            raise IndexError("Cannot push back an element beyond the reserved range.")
+        if self._i_elem_end >= self._elements.size:
+            raise IndexError("Cannot push an element to the end beyond the reserved range.")
 
-        self._elements[self._i_elem] = elem
-        self._i_elem += 1
-
+        self._elements[self._i_elem_end] = elem
+        self._i_elem_end += 1
+    
+    def pop_back(self) -> T:
+        if self._i_elem_end == self._i_elem_start:
+            raise IndexError("Cannot pop the element off the end; there are no elements.")
+        
+        ret_value = self._elements[self._i_elem_end - 1]
+        self._i_elem_end -= 1
+        
+        return ret_value
+    
+    def push_front(self, elem: T) -> None:
+        if self._i_elem_start == 0:
+            raise IndexError("Cannot push an element to the start beyond the reserved range.")
+        
+        self._i_elem_start -= 1
+        self._elements[self._i_elem_start] = elem
+    
+    def pop_front(self) -> T:
+        if self._i_elem_start == self._i_elem_end:
+            raise IndexError("Cannot pop the element off the start; there are no elements.")
+        
+        ret_value = self._elements[self._i_elem_start]
+        self._i_elem_start += 1
+        
+        return ret_value
+            
     @property
     def elements(self) -> np.ndarray[T]:
         return self._elements
