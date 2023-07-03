f513cba846cd905a145e624e91608b1e681fdb71
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-07-03 17:36:17 -0400

    Add much more functionality to ReservedDeque

--- src/nn_fourbody_potential/reserved_deque.py
+++ src/nn_fourbody_potential/reserved_deque.py
@@ -6,7 +6,10 @@ This is similar to how in C++, one can create a `std::vector`, call `reserve()`,
 call `push_back()` without having to reallocate memory.
 """
 
+from typing import Any
 from typing import Generic
+from typing import Iterator
+from typing import Optional
 from typing import Sequence
 from typing import TypeVar
 from typing import Union
@@ -17,11 +20,33 @@ import numpy as np
 T = TypeVar("T")
 
 
+# TODO: add method to "reset" the ReservedVector without having to actually clear out the elements;
+#       the "effective size" of the vector is determined by `self._i_elem_start` and `self._i_elem_end`,
+#       and as long as the total size remains the same, we can "reset" the vector just be setting both
+#       of those values to 0
+
+
 class ReservedDeque(Generic[T]):
-    def __init__(self, elements: np.ndarray[T], i_elem_start: int, i_elem_end: int) -> None:
+    def __init__(
+        self, elements: np.ndarray[T], i_elem_start: Optional[int] = None, i_elem_end: Optional[int] = None
+    ) -> None:
         self._elements = elements
-        self._i_elem_start = i_elem_start
-        self._i_elem_end = i_elem_end
+
+        if i_elem_start is None:
+            self._i_elem_start = 0
+        else:
+            self._i_elem_start = i_elem_start
+
+        if i_elem_end is None:
+            self._i_elem_end = elements.shape[0]
+        else:
+            self._i_elem_end = i_elem_end
+
+        if self._i_elem_start < 0:
+            raise ValueError("The starting index must be 0 or greater.")
+
+        if self._i_elem_start > elements.shape[0]:
+            raise ValueError("The ending index cannot exceed the length of the underlying numpy array.")
 
     @classmethod
     def new(cls, shape: Union[int, Sequence[int]], type_t: T) -> "ReservedDeque[T]":
@@ -39,6 +64,19 @@ class ReservedDeque(Generic[T]):
         i_elem_start = 0
         i_elem_end = len(elements)
         return cls(elements, i_elem_start, i_elem_end)
+
+    def reset(self) -> None:
+        """
+        Effectively clears the data structure, allowing the user to act as if it were empty.
+
+        The reason this works, is that all the functionality of this data structure, such as
+        how it allows elements to be pushed and popped off either end, its size, and random
+        access, all depend on the starting and ending indices.
+
+        We can thus effectively "empty" the underlying data by just setting both indices to 0.
+        """
+        self._i_elem_start = 0
+        self._i_elem_end = 0
 
     def push_back(self, elem: T) -> None:
         if self._i_elem_end >= self._elements.size:
@@ -77,8 +115,47 @@ class ReservedDeque(Generic[T]):
         return self._elements
 
     @property
-    def size(self) -> int:
+    def max_size(self) -> int:
         return self._elements.size
 
+    @property
+    def size(self) -> int:
+        return self._i_elem_end - self._i_elem_start
+
+    def __iter__(self) -> Iterator:
+        self._i_iter = self._i_elem_start
+        return self
+
+    def __next__(self) -> T:
+        if self._i_iter >= self._i_elem_end:
+            raise StopIteration
+
+        value = self._elements[self._i_iter]
+        self._i_iter += 1
+
+        return value
+
+    def __eq__(self, other: Any) -> bool:
+        # NOTE: `Subscripted generics cannot be used with class and instance checks`; so I cannot use
+        #       `ReservedDeque[T]` here, and I have to settle for just `ReservedDeque`
+        if not isinstance(other, ReservedDeque):
+            return NotImplemented
+
+        return (
+            self.max_size == other.max_size
+            and self.size == other.size
+            and self._i_elem_start == other._i_elem_start
+            and self._i_elem_end == other._i_elem_end
+            and all([self_val == other_val for (self_val, other_val) in zip(self, other)])
+        )
+
     def __getitem__(self, index: int) -> T:
+        if not self._i_elem_start <= index < self._i_elem_end:
+            raise IndexError(
+                "Element access is out of bounds. Must be `lower index` <= index < `upper index`\n"
+                f"lower index: {self._i_elem_start}\n"
+                f"upper index: {self._i_elem_end}\n"
+                f"index: {index}"
+            )
+
         return self._elements[index]
--- tests/test_reserved_deque.py
+++ tests/test_reserved_deque.py
@@ -1,5 +1,7 @@
 import pytest
 
+import numpy as np
+
 from nn_fourbody_potential.reserved_deque import ReservedDeque
 
 
@@ -15,12 +17,20 @@ def default_deque_246() -> ReservedDeque[int]:
 
 class TestReservedDeque:
     def test_basic(self, default_deque_246: ReservedDeque[int]):
-        size = 3
-
         elements = default_deque_246.elements
         assert elements[0] == 2
         assert elements[1] == 4
         assert elements[2] == 6
+
+    def test_classmethod_from_array(self):
+        expected = ReservedDeque[int].new(3, int)
+        expected.push_back(123)
+        expected.push_back(456)
+        expected.push_back(789)
+
+        actual = ReservedDeque[int].from_array(np.array([123, 456, 789]))
+
+        assert expected == actual
 
     def test_push_back_matches_pop_back(self, default_deque_246: ReservedDeque[int]):
         assert default_deque_246.pop_back() == 6
@@ -42,6 +52,11 @@ class TestReservedDeque:
         assert default_deque_246.pop_front() == 2
         assert default_deque_246.pop_back() == 4
 
+    def test_pop_front_then_push_front(self, default_deque_246: ReservedDeque[int]):
+        assert default_deque_246.pop_front() == 2
+        default_deque_246.push_front(123)
+        assert default_deque_246.pop_front() == 123
+
     def test_raises_push_back_too_many(self, default_deque_246: ReservedDeque[int]):
         with pytest.raises(IndexError):
             default_deque_246.push_back(8)
@@ -61,3 +76,58 @@ class TestReservedDeque:
 
         with pytest.raises(IndexError):
             default_deque_246.pop_front()
+
+    def test_max_size(self):
+        max_size = 3
+        deque = ReservedDeque[int].new(max_size, int)
+        assert deque.max_size == max_size
+
+    @pytest.mark.parametrize("max_size, elements_to_push, expected_size", [(3, [], 0), (3, [0], 1), (3, [123, 456], 2)])
+    def test_size_with_push_back(self, max_size, elements_to_push, expected_size):
+        deque = ReservedDeque[int].new(max_size, int)
+        for elem in elements_to_push:
+            deque.push_back(elem)
+
+        assert deque.size == expected_size
+
+    def test_size_with_push_back_and_pop_front(self):
+        deque = ReservedDeque[int].new(10, int)
+        deque.push_back(1)
+        deque.push_back(2)
+        deque.push_back(3)
+        deque.pop_front()
+
+        assert deque.size == 2
+
+    def test_dunder_getitem(self):
+        deque = ReservedDeque[int].new(10, int)
+        deque.push_back(123)
+        deque.push_back(456)
+        deque.push_back(789)
+
+        assert deque[0] == 123
+        assert deque[1] == 456
+        assert deque[2] == 789
+
+    def test_raises_dunder_getitem_past_end(self, default_deque_246):
+        with pytest.raises(IndexError):
+            default_deque_246[3]
+
+    def test_raises_dunder_getitem_past_end(self, default_deque_246):
+        default_deque_246.pop_front()
+        with pytest.raises(IndexError):
+            default_deque_246[0]
+
+    def test_reset(self):
+        deque = ReservedDeque[float].new(10, float)
+        deque.push_back(1.1)
+        deque.push_back(2.2)
+        deque.push_back(3.3)
+
+        assert deque.max_size == 10
+        assert deque.size == 3
+
+        deque.reset()
+
+        assert deque.max_size == 10
+        assert deque.size == 0
