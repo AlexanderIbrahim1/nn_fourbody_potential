4a75485df57c6de44f365578d0bb6fa0c0b1a307
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-09-11 15:39:55 -0400

    Add option to construct ErrorWriter with an append instead of write mode

--- src/nn_fourbody_potential/modelio/error_writer.py
+++ src/nn_fourbody_potential/modelio/error_writer.py
@@ -6,13 +6,18 @@ class ErrorWriter:
 
     _savefile: Path
 
-    def __init__(self, savepath: Path, filename: str) -> None:
+    def __init__(self, savepath: Path, filename: str, mode: str) -> None:
         if not savepath.exists():
             savepath.mkdir()
 
         self._savefile = Path(savepath, filename)
-        with open(self._savefile, "w") as fout:
-            fout.write("[Epoch]   [Error]\n")
+
+        if mode not in ["w", "a"]:
+            raise ValueError("The only accepted writing modes for the error file are 'w' and 'a'.\n" f"Found: {mode}")
+
+        if mode == "w":
+            with open(self._savefile, "w") as fout:
+                fout.write("[Epoch]   [Error]\n")
 
     def append(self, epoch: int, error: float) -> None:
         with open(self._savefile, "a") as fout:
