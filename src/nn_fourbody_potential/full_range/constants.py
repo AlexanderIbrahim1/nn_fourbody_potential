e53c07b658537f3e620e18f85cb63810b3312863
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2024-05-19 00:56:17 -0400

    Increase minimum cutoff distance from abinitio-bade transition

--- src/nn_fourbody_potential/full_range/constants.py
+++ src/nn_fourbody_potential/full_range/constants.py
@@ -5,5 +5,5 @@ falls into the short-range, mid-range, or long-range categories.
 
 SHORT_RANGE_SCALING_STEP = 0.05
 SHORT_RANGE_DISTANCE_CUTOFF = 2.2
-LOWER_MIXED_DISTANCE = 3.8
+LOWER_MIXED_DISTANCE = 4.0
 UPPER_MIXED_DISTANCE = 4.5
