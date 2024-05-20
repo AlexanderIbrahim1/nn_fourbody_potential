106aed7ed0e4135f5263bbca6bfcbc101b1879f1
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2024-05-20 13:18:16 -0400

    Modify the contants for the extrapolation

--- src/nn_fourbody_potential/full_range/constants.py
+++ src/nn_fourbody_potential/full_range/constants.py
@@ -3,8 +3,8 @@ This module contains constants relevant to determining whether a four-body geome
 falls into the short-range, mid-range, or long-range categories.
 """
 
-SHORT_RANGE_SCALING_STEP = 0.05
+SHORT_RANGE_SCALING_STEP = 0.01
 LOWER_SHORT_DISTANCE = 2.2
-UPPER_SHORT_DISTANCE = 2.3
+UPPER_SHORT_DISTANCE = 2.25
 LOWER_MIXED_DISTANCE = 4.0
 UPPER_MIXED_DISTANCE = 4.5
